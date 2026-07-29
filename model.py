66959512d5d52f670cffc7e750ee3c318ba38007
Mohamed Aziz Ayari <139351844+Asembris@users.noreply.github.com>
2026-07-29 14:05:33 +0200

    Step 58: build_uniform_smoothing_distribution

--- model.py
+++ model.py
@@ -608,8 +608,14 @@ def compute_noam_learning_rate(step, d_model, warmup_steps):
     lr=d_model**(-1/2)*min(step**(-1/2),step*(warmup_steps**(-3/2)))
     return lr
 
-# Step 58 - build_uniform_smoothing_distribution (not yet solved)
-# TODO: implement
+# Step 58 - build_uniform_smoothing_distribution
+import torch
+
+def build_uniform_smoothing_distribution(shape, vocab_size, epsilon):
+    # TODO: return a float tensor of `shape` filled with epsilon / (vocab_size - 2).
+    const=epsilon/(vocab_size-2)
+    res=torch.full(shape,const)
+    return res
 
 # Step 59 - set_confidence_on_gold_tokens (not yet solved)
 # TODO: implement
