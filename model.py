87d54297eeb4161c50a2799d1790bdaa5d3420b0
Mohamed Aziz Ayari <139351844+Asembris@users.noreply.github.com>
2026-07-30 20:56:10 +0200

    Step 79: mark_finished_beams

--- model.py
+++ model.py
@@ -910,8 +910,12 @@ def append_tokens_to_beam_sequences(beam_sequences, beam_indices, token_ids):
     res=torch.cat((parent_sequences,new_tokens),dim=-1)
     return res
 
-# Step 79 - mark_finished_beams (not yet solved)
-# TODO: implement
+# Step 79 - mark_finished_beams
+import torch
+
+def mark_finished_beams(token_ids, finished_flags, end_token_id):
+    # TODO: return updated boolean finished flags for each beam given the new token ids
+    return finished_flags|(token_ids==end_token_id)
 
 # Step 80 - select_best_finished_beam (not yet solved)
 # TODO: implement
